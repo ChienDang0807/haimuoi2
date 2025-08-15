897d0f6e9db3c689d9ef72c8304df475d47ff46f
BTL_Nhom12_Android <dangthochien0807@gmail.com>
2025-08-15 16:08:11 +0700

    update payment webhook

--- payment/src/main/java/vn/chiendt/dto/request/AdvanceSearchRequest.java
+++ payment/src/main/java/vn/chiendt/dto/request/AdvanceSearchRequest.java
@@ -1,7 +1,15 @@
 package vn.chiendt.dto.request;
 
+import jakarta.validation.ValidationException;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.util.StringUtils;
+import vn.chiendt.common.Currency;
+import vn.chiendt.common.Operation;
+import vn.chiendt.common.PaymentMethod;
+import vn.chiendt.common.TransactionStatus;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class AdvanceSearchRequest {
@@ -11,4 +19,56 @@ public class AdvanceSearchRequest {
     private String sort;
     private int page;
     private int size;
+
+    public void validate() {
+        for (SearchField<?> field : searchFields) {
+            String fieldName = field.getField();
+            Operation operation = field.getOperation();
+
+            if (!StringUtils.hasText(fieldName)) {
+                throw new ValidationException("field must not be blank");
+            }
+            if (operation == null) {
+                throw new ValidationException("operation must not be null");
+            }
+            if (null == field.getValue()) {
+                throw new ValidationException("value must not be null");
+            }
+
+            List<String> numbers = List.of("customerId", "amount");
+            if (numbers.contains(fieldName)) {
+                if (!(field.getValue() instanceof Number)) {
+                    throw new ValidationException(fieldName + " must be a number");
+                }
+            }
+
+            List<String> strings = List.of("paymentId", "description");
+            if (strings.contains(fieldName)) {
+                if (!(field.getValue() instanceof String)) {
+                    throw new ValidationException(fieldName + " must be a String");
+                }
+            }
+
+            if (fieldName.equals("paymentMethod") || fieldName.equals("currency") || fieldName.equals("status")) {
+                try {
+                    PaymentMethod.valueOf(String.valueOf(field.getValue()).toUpperCase());
+                    Currency.valueOf(String.valueOf(field.getValue()).toUpperCase());
+                    TransactionStatus.valueOf(String.valueOf(field.getValue()).toUpperCase());
+                } catch (Exception e) {
+                    throw new ValidationException(fieldName + " invalid");
+                }
+            }
+
+            List<String> dates = List.of("createdAt", "updatedAt");
+            if (dates.contains(fieldName)) {
+                String dateFormat = "dd/MM/yyyy";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+                try {
+                    LocalDate date = LocalDate.parse(field.getValue().toString(), formatter);
+                } catch (Exception e) {
+                    throw new ValidationException(fieldName + " invalid format");
+                }
+            }
+        }
+    }
 }
